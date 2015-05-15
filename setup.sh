10c11c689afb2e463d87bf08b7b8b79ae8438a9a
soulgalore <peter@soulgalore.com>
2015-05-15 20:51:20 +0200

    env needed

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export DATABASE_HOST=
+export DATABASE_PASSWORD=
+export DATABASE_USER=sitespeedio
+
+export REDIS_HOST=
+export REDIS_QUEUES=
+export REDIS_PASSWORD=
+export REDIS_RESULT_QUEUE=result
+
+export S3_ACCESS_KEY_ID=
+export S3_SECRET_ACCESS_KEY=
