cbe93f09577bf6fef4f92643a1bcd69935a3d263
soulgalore <peter@soulgalore.com>
2015-05-20 00:13:19 +0200

    dockerize

---
+++ docker/server/scripts/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+## ADD your environment variables before you start
+
+
+nodejs /usr/lib/node_modules/@run.sitespeed.io/server/app.js
---
+++ docker/worker/scripts/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+## ADD your environment variables before you start
+
+nodejs /usr/lib/node_modules/@run.sitespeed.io/receiver/worker/app.js
