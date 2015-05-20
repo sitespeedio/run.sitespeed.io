3cc5136446aef68ea520c18518cf4358e7947720
soulgalore <peter@soulgalore.com>
2015-05-20 21:58:37 +0200

    updates

--- docker/receiver/scripts/start.sh
+++ docker/receiver/scripts/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ## ADD your environment variables before you start
 
-
-nodejs /usr/lib/node_modules/@run.sitespeed.io/receiver/lib/app.js
+run.sitespeed.io-receiver
--- docker/server/scripts/start.sh
+++ docker/server/scripts/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ## ADD your environment variables before you start
 
-
-nodejs /usr/lib/node_modules/@run.sitespeed.io/server/app.js
+run.sitespeed.io-server
