ada418dc8e37e8e8469e450c857c85d2a46548c0
soulgalore <peter@soulgalore.com>
2015-05-26 01:49:25 +0200

    ip fix

--- db/setup.sql
+++ db/setup.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS runs (
 id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 testId CHAR (37),
 url VARCHAR(2083),
-testerIp VARCHAR(40) NOT NULL,
+testerIp VARCHAR(20) NOT NULL,
 created DATETIME DEFAULT CURRENT_TIMESTAMP,
 changed DATETIME ON UPDATE CURRENT_TIMESTAMP,
 location ENUM('sf', 'nyc', 'london', 'amsterdam','singapore','frankfurt'),
