e77e87bd2c32d1f51c03991d9a08b5630d480fbe
soulgalore <peter@soulgalore.com>
2015-07-13 16:43:31 +0200

    slack setup

--- setup.sh
+++ setup.sh
@@ -10,3 +10,9 @@ export REDIS_RESULT_QUEUE=result
 
 export S3_ACCESS_KEY_ID=
 export S3_SECRET_ACCESS_KEY=
+
+export SLACK_DOMAIN=
+export SLACK_TOKEN=
+export SLACK_LEVEL=
+export SLACK_CHANNEL=
+export SLACK_USERNAME=
