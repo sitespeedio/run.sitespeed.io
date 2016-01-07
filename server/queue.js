c99f83babc9e6fba1ed3c017e4ec7225912f4742
Tobias Lidskog <tobias@lidskog.se>
2016-01-07 13:43:51 +0100

    Check for errors from queue#getQueueAttributes.

--- server/queue.js
+++ server/queue.js
@@ -68,7 +68,7 @@ module.exports = {
 
 				rsmq.getQueueAttributes({'qname': queue}, function(error, data) {
 					log.debug('We have ' + JSON.stringify(data.msgs) + ' in the queue');
-					cb(err, resp, data.msgs);
+					cb(error, resp, data.msgs);
 				});
 
 			} else {
